43c4a05eb4b07324facf1c79e9ac8d19a25a5e95
Mahesh Gurrapu <mgurrapu@prokarma.com>
2020-02-05 12:45:57 +0530

    Code refactor

--- src/main/java/com/prokarma/customer/publisher/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/prokarma/customer/publisher/service/impl/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.prokarma.customer.publisher.service.impl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import com.google.gson.Gson;
@@ -19,10 +20,13 @@ public class CustomerServiceImpl implements CustomerService {
   @Autowired
   private Gson jsonConverter;
 
+  @Value("${kafka.customer.topic.name}")
+  private String customerTopic;
+
   @Override
   public void publishToKafka(Customer customer) {
     String customerJson = jsonConverter.toJson(customer);
-    kafkaTemplate.send("customer_topic", customerJson).addCallback(
+    kafkaTemplate.send(customerTopic, customerJson).addCallback(
         result -> LOGGER.info(String.format("Sent message=[%s] with offset=[%s]", customerJson,
             result.getRecordMetadata().offset())),
         ex -> LOGGER.error(String.format("Unable to send message=[%s] due to : %s", customerJson,
