b8576f7fc983937f611a2658ce83197338bd6210
Mahesh Gurrapu <mgurrapu@prokarma.com>
2020-02-18 11:01:57 +0530

    added dev properties fiel

--- src/main/java/com/prokarma/customer/publisher/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/prokarma/customer/publisher/exception/RestResponseEntityExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -38,5 +39,15 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     exceptionResponse.setPath(request.getDescription(false));
     return exceptionResponse;
   }
+
+  @ExceptionHandler(Exception.class)
+  protected ResponseEntity<Object> handleAllExceptions(RuntimeException ex, WebRequest request) {
+
+    return handleExceptionInternal(ex,
+        createErrorResponse(request, HttpStatus.INTERNAL_SERVER_ERROR,
+            "Exception in processing data."),
+        new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+  }
+
 
 }
